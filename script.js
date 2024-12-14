c96a9d66f1018ba54a4047cc6880b856d5158305
vinod <guptavinod22111@gmail.com>
2024-12-14 12:00:56 +0530

    calculator

---
+++ script.js
@@ -0,0 +1,14 @@
+let display=document.querySelector("#display")
+function Sinf(){
+    display.value=Math.sin(display.value)
+}
+function Cosf(){
+    display.value=Math.cos(display.value)
+}
+function Tanf(){
+    display.value=Math.tan(display.value)
+}
+function Sqrf(){
+    display.value=Math.sqrt(display.value)
+}
+
